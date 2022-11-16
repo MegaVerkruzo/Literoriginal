9d3047c7725219badc6743cc49ee9bac3cf520ee
Alexey Grunskii <you@example.com>
2022-11-16 14:47:50 +0300

    Write base of application

---
+++ src/main/java/com/example/liter/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.liter;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "hello, letsCode");
+        return "main";
+    }
+}
