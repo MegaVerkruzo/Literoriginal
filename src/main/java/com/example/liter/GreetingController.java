3f200f6e04781f40491c208459ace366963badb1
Alexey Grunskii <you@example.com>
2022-11-17 01:13:36 +0300

    change returning thing in greeting controller and add form in main.mustache

--- src/main/java/com/example/liter/GreetingController.java
+++ src/main/java/com/example/liter/GreetingController.java
@@ -3,6 +3,9 @@ package com.example.liter;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.example.liter.domain.Message;
+import com.example.liter.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
@@ -18,7 +24,10 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "hello, letsCode");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
         return "main";
     }
 }
